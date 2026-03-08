fe2b58c22e630d9b393189f99ea2b37f3fc5189a
suwandre <suwandresukijat@gmail.com>
2026-03-08 12:43:52 +0100

    fix: background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-background text-foreground min-h-screen`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
